11fa348ee70881b0abcdb32764a6e63969c4435e
Aquarius2301 <125976902+Aquarius2301@users.noreply.github.com>
2023-02-27 18:10:07 +0700

    Create Node.java

---
+++ DoublyLinkedList/Node.java
@@ -0,0 +1,73 @@
+public class Node {
+
+    private int value; // the value of a node
+    private Node next; // the next node
+    private Node prev; // the previous node
+
+    /**
+     * Create a node
+     * <p>
+     * A node contains value and the next node
+     * 
+     * @param value the value
+     */
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    /**
+     * get value of a node
+     * 
+     * @return value of a node
+     */
+    public int getValue() {
+        return this.value;
+    }
+
+    /**
+     * Set the value of a node
+     * 
+     * @param value the value
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * get next node of a node
+     * 
+     * @return next node of a node
+     */
+    public Node getNext() {
+        return this.next;
+    }
+
+    /**
+     * Set the next node of a node
+     * 
+     * @param next the next node
+     */
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    /**
+     * get the previous node of a node
+     * 
+     * @return the previous node
+     */
+    public Node getPrev() {
+        return this.prev;
+    }
+
+    /**
+     * Set the previous node of a node
+     * 
+     * @param next the previous node
+     */
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+}
